6bc4193636b2ab0683c4371f0e89cdbbee80515e
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2017-11-27 20:20:08 -0200

    Refactory tail

--- src/f.php
+++ src/f.php
@@ -41,21 +41,7 @@ function head($xs) {
 
 // [a] -> [a]
 function tail($xs) : array {
-
-    if (!$xs) {
-        return [];
-    }
-
-    $result = [];
-    $counter = 1;
-    foreach($xs as $key => $entry) {
-        if ($counter != 1) {
-            $result[$key] = $entry;
-        }
-        $counter++;
-    }
-
-    return $result;
+    return array_slice($xs, 1);
 }
 
 
